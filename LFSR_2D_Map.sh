db7eb577b6c361abfece9bc044bca5b94d72220f
codingbychanche <Berthold@Bertholds-Apple-II.local>
2016-03-09 22:17:21 +0100

    Blocked exits now are shown correctly. Added inventory.

--- LFSR_2D_Map.sh
+++ LFSR_2D_Map.sh
@@ -16,13 +16,18 @@ scene=2
 
 # Map is 10x10 squares
 
-mapx=10
-mapy=10 
+mapx=20
+mapy=20 
 
 # Pos of the lost bit
 
 bitx=2
 bity=10
+
+# Players inventory
+
+water=5
+fire=5
 
 # Pos. of player on map 
 #
@@ -88,6 +93,7 @@ descripe()
     echo
     echo You are in room x/y $roomx/$roomy room number:$roomnumber 
     echo Your room looks like:$scene
+    
 
     # 'scene', first 8- Bits:
     #
@@ -115,7 +121,7 @@ descripe()
     east=6
     ladder=7
     lake=5
-    stream=3
+    fire=3
     
     clue=8
 
@@ -179,13 +185,13 @@ descripe()
 	dl=1
     fi
 
-    # If the room has no exits, then build one to prevent the plyer
+    # If the room has no exits, then build one to prevent the player
     # from running into deathends
 
     let "exits=$dn+$ds+$de+$dw+$dl"
     if [ $exits -eq 0 ]
     then	
-	echo Found a hidden path to the north and to the south
+	echo Found a hidden path running from north to south
 	dn=1
 	ds=1
     fi
@@ -195,13 +201,15 @@ descripe()
     let "d=$lake&$scene"
     if [ $d -eq $lake ]
     then
-	echo I can see a lake
+	echo I can see a lake. Water reffiled!
+	water=5
     fi
 
-    let "d=$stream&$scene"
-    if [ $d -eq $stream ]
+    let "d=$fire&$scene"
+    if [ $d -eq $fire ]
     then
-	echo I can see a stream
+	echo I can see a fireplace. Your torch burns brightly again!
+	fire=5
     fi
 
     # A clue?
@@ -212,7 +220,16 @@ descripe()
 	echo A secret scroll is here. It is written that the lost bit is 
 	
 	let "dx=$roomx-$bitx"
+	if [ $dx -lt 0 ]
+	then
+	    let "dx=dx * - 1"
+	fi
+
 	let "dy=$roomy-$bity"
+	if [ $dy -lt 0 ]
+	then
+	    let "dy=dy * - 1"
+	fi
 
 	if [ $dx -lt 0 ]
 	then 
@@ -231,6 +248,21 @@ descripe()
 	echo $dx steps $dvx from here and $dy steps $dvy of here
 	echo Good luck!
     fi
+
+    # Inventory
+
+    echo Water: $water
+    if [ $water -lt 3 ]
+    then 
+	echo You are thirsty
+    fi
+
+    echo Torch: $fire
+    if [ $fire -lt 3 ]
+    then
+	echo Your torch seems to fade........
+    fi
+
     
 }
 
@@ -252,19 +284,22 @@ echo Below the map is a dungeon which. You can access the dungeon through the pi
 echo
 echo You, the brave player start at location $roomx/$roomy
 echo
-echo You move by telling me the desiered direction which is either north or south, west or east 
+echo You move by telling me the desired direction which is either north or south, west or east 
 echo If you can see a pit you can go up or down. Use the first letter of the direction you want to
 echo travel: n,s,e,w,u or d
 echo
-echo In the dungeon you move two rooms at a time
+echo To get a description of your room you can type: l
+echo
+
+# Descripe start room
+
+descripe
 
 ##############################
 # Main loop
 #
 # Player movement
 ##############################
-
-
 
 while [ 1 ]
 do
@@ -279,13 +314,7 @@ do
 	
 	break
     fi
-    
-
-
-    # Descripe room
-
-    descripe
-
+   
     # Prompt
     
     echo --------------Which way n/s/e/w ?
@@ -293,59 +322,96 @@ do
 
     # Move
     
+    # West look?
+
+    if [ $dir = "l" ] 
+    then
+	echo Examining......
+	descripe
+    fi
+
     # West ok?
-    
-    if [ $dir == "w" ] && [ $dw == 1 ] 
+
+    if [ $dir = "w" ] 
     then
 	if [ $roomx -gt 0 ]
 	then
+	    if [ $dw -eq 1 ] 
+	    then
 	    increase
 	    let "roomx=roomx-1"
+	    descripe
+	    else
+		echo West is blocked!
+	    fi	
 	fi
-    else
-	echo West exit  is blocked
     fi
     
     # East ok?
     
-    if [ $dir == "e" ] && [ $de == 1 ]
+    if [ $dir = "e" ] 
     then
 	if [ $roomx -lt $mapx ]
 	then
-	    decrease
-	    let "roomx=roomx+1"
-	    
-	fi
-    else
-	echo East is blocked
+	    if [ $de -eq 1 ]
+	    then
+		decrease
+		let "roomx=roomx+1"
+		descripe
+	    else
+		echo East is blocked
+	    fi
+        fi
     fi
     
     # North ok?
     
-    if [ $dir == "n" ] && [ $dn == 1 ]
+    if [ $dir = "n" ] 
     then
 	if [ $roomy -gt 0 ]
 	then
-	    let "scene=scene-1"
-	    let "roomy=roomy-1"
-	    
-	fi
-    else
-	echo North is blocked
+	    if [ $dn -eq 1 ]
+	    then
+		let "scene=scene-1"
+		let "roomy=roomy-1"
+		descripe
+	    else
+		echo North is blocked!
+	    fi
+	fi	
+     
     fi
     
     # South ok?
     
-    if [ $dir == "s" ] && [ $ds == 1 ]
+    if [ $dir = "s" ] 
     then
 	if [ $roomy -lt $mapy ]
 	then
-	    let "scene=scene+1"
-	    let "roomy=roomy+1"
-	    
+	    if  [ $ds -eq 1 ]
+	    then
+		let "scene=scene+1"
+		let "roomy=roomy+1"
+		descripe
+	    else
+		echo South is blocked!
+	    fi
 	fi
+    fi
+
+    # Check inventory
+
+    if [ $fire -ge 0 ]
+    then
+	let "fire=fire-1"
+    fi
+
+    if [ $water -gt 0 ]
+    then
+	let "water=water-1"
     else
-	echo South is blocked
+	echo You run out of water! You are death! The lost bit is lost forever....
+	break
     fi
 done
 
